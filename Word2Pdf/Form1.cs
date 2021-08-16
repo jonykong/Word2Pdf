d56cd547b3ab84bd6bb99590d828681756e20188
1114test <846039980@qq.com>
2021-08-16 17:14:45 +0800

    项目提交

---
+++ Word2Pdf/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Word2Pdf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 选择文件
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;//该值确定是否可以选择多个文件
+            dialog.Title = "请选择doc";
+            dialog.Filter = "PPT |*.docx;*doc";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string file = dialog.FileName;
+                this.textBox1.ResetText();
+                this.textBox1.AppendText(file);
+            }
+        }
+
+        // 开始
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string filePath = this.textBox1.Text;
+           
+            if (filePath.EndsWith(".doc") || filePath.EndsWith(".docx"))
+            {
+                Process(filePath);
+                string filedir = Path.GetDirectoryName(filePath);
+                System.Diagnostics.Process.Start("explorer.exe", filedir);
+            }
+        }
+        public static void Process(string file)
+        {
+            Console.WriteLine("Found: {0}", file);
+            File.SetAttributes(file, FileAttributes.Normal);
+            var idx = file.LastIndexOf(".");
+            var newPath = file.Remove(idx, file.Length - idx) + ".pdf";
+            Doc2PDFAtServerClass.word2PdfFcih(file, newPath);
+        }
+    }
+}
---
+++ Word2Pdf/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*using System.IO;*/
+using System.Windows.Forms;
+
+namespace Word2Pdf
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+//         static void Main(string[] args)
+//         {
+//             if (args.Length != 1)
+//             {
+//                 PrintUsage();
+//                 return;
+//             }
+// 
+//             var path = args[0];
+//             if (Directory.Exists(path))
+//             {
+//                 Run(path);
+//             }
+//             else if(path.EndsWith(".docx") || path.EndsWith(".doc"))
+//             {
+//                 Process(path);
+//             }
+//             else
+//             {
+//                 Console.WriteLine("{0} is not a directory");
+//             }
+//         }
+
+//         private static void PrintUsage()
+//         {
+//             Console.WriteLine("Pass the directory as a param");
+//         }
+
+//         public static void Run(string path)
+//         {
+//             var files = Directory.GetFiles(path, "*.docx").Union(Directory.GetFiles(path, "*.doc"));
+//             foreach (var file in files)
+//             {
+//                 if (!Path.GetFileName(file).StartsWith("~"))
+//                 {
+//                     Process(file);
+//                 }
+//             }
+// 
+//             var subs = Directory.GetDirectories(path);
+//             foreach (var sub in subs)
+//             {
+//                 Run(sub);
+//             }
+//         }
+
+//         public static void Process(string file)
+//         {
+//             Console.WriteLine("Found: {0}", file);
+//             File.SetAttributes(file, FileAttributes.Normal);
+//             var idx = file.LastIndexOf(".");
+//             var newPath = file.Remove(idx, file.Length - idx) + ".pdf";
+//             Doc2PDFAtServerClass.word2PdfFcih(file, newPath);
+//         }
+    }
+}
